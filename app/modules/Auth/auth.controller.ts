ca583d13ede0d1fe93ab83218d08c4b13e0a5677
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-06-25 02:48:31 +0600

    update auth controller

--- app/modules/Auth/auth.controller.ts
+++ app/modules/Auth/auth.controller.ts
@@ -4,14 +4,12 @@ import asyncCatch from "../../utils/try.code";
 import config from "../../config";
 
 
-
-
 const Login_User_Controller = asyncCatch(async (req: Request, res: Response) => {
     const result = await Auth_Services.login_User_Service(req.body);
-    const {accessToken,refreshToken,needPassChange} = result;
+    const { accessToken, refreshToken, needPassChange } = result;
 
-    res.cookie('refreshToken',refreshToken,{
-        secure: config.sds!=='DEVELOPMENT',
+    res.cookie('refreshToken', refreshToken, {
+        secure: config.sds !== 'DEVELOPMENT',
         httpOnly: true
     })
 
@@ -26,20 +24,20 @@ const Login_User_Controller = asyncCatch(async (req: Request, res: Response) => 
 })
 const Change_Pass_Controller = asyncCatch(async (req: Request, res: Response) => {
 
-    const result = await Auth_Services.change_pass_controller(req.user,req.body);
+    const result = await Auth_Services.change_pass_controller(req.user, req.body);
     res.status(200).json({
         success: true,
         message: "Successfully Login User !",
         data: result,
     })
 })
-const Refresh_token_cotroller = asyncCatch(async(req: Request, res: Response)=>{
+const Refresh_token_cotroller = asyncCatch(async (req: Request, res: Response) => {
 
-    const result = await Auth_Services.Refresh_Token_Service(req.cookies.refreshToken)
+    const { accessToken } = await Auth_Services.Refresh_Token_Service(req.cookies.refreshToken)
     res.status(200).json({
         success: true,
         message: "Successfully Login User !",
-        data: result,
+        data: accessToken,
     })
 })
 
